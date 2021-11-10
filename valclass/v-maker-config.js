9d619470cd110ea8b0b57a01bd159bd69a776f47
Muhammad Khoirul Huda <79193720+valclassdevelop@users.noreply.github.com>
2021-11-10 15:20:01 +0700

    Add files via upload

---
+++ valclass/v-maker-config.js
@@ -0,0 +1,20 @@
+createMakerUtilitiesName({
+    createUtilities : [
+        "bg1",
+        "bg2",
+        "bg3",
+        "color1"
+      ],
+    createValueUtilities : [
+        "background-color: #090829",
+        "background-color: #dc3545",
+        "background-color: #0d6efd",
+        "color: #090829",
+      ],
+    createBreakPointsUtilities : [
+        "df",
+        "df",
+        "df",
+        "df",
+      ],
+})
